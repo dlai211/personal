44ddd95a5bf22254760df2f35c1283ff927f3688
dlai <72419337+dlai211@users.noreply.github.com>
2024-08-27 14:34:44 +0800

    Update script.js

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -44,9 +44,22 @@ function handleClick(e) {
   } else {
     swapTurns()
     setBoardHoverClass()
+
+    // Computer makes its move
+    if (circleTurn) {
+      const bestMove = minimax([...cellElements], CIRCLE_CLASS).index;
+      placeMark(cellElements[bestMove], CIRCLE_CLASS);
+      if (checkWin(CIRCLE_CLASS)) {
+        endGame(false);
+      } else if (isDraw()) {
+        endGame(true);
+      } else {
+        swapTurns();
+        setBoardHoverClass();
+      }
+    }
   }
 }
-
 function endGame(draw) {
   if (draw) {
     winningMessageTextElement.innerText = 'Draw!'
@@ -86,4 +99,80 @@ function checkWin(currentClass) {
       return cellElements[index].classList.contains(currentClass)
     })
   })
+}
+
+function minimax(minimax_board, depth, is_maximizing) {
+  if (checkWin(CIRCLE_CLASS)) {
+    return Infinity;
+  } else if (checkWin(X_CLASS)) {
+    return -Infinity;
+  } else if (isDraw()) {
+    return 0;
+  }
+
+  if (is_maximizing) {
+    let best_score = -1000;
+    for (let i = 0; i < 9; i++) {
+      cellElements[i].classList.addclassList.contains()
+    }
+  }
+}function minimax(newBoard, player) {
+  const availableSpots = getAvailableSpots(newBoard);
+
+  if (checkWin(X_CLASS)) {
+    return { score: -10 };
+  } else if (checkWin(CIRCLE_CLASS)) {
+    return { score: 10 };
+  } else if (availableSpots.length === 0) {
+    return { score: 0 };
+  }
+
+  const moves = [];
+  for (let i = 0; i < availableSpots.length; i++) {
+    const move = {};
+    move.index = availableSpots[i];
+    newBoard[availableSpots[i]].classList.add(player);
+
+    if (player === CIRCLE_CLASS) {
+      const result = minimax(newBoard, X_CLASS);
+      move.score = result.score;
+    } else {
+      const result = minimax(newBoard, CIRCLE_CLASS);
+      move.score = result.score;
+    }
+
+    newBoard[availableSpots[i]].classList.remove(player);
+    moves.push(move);
+  }
+
+  let bestMove;
+  if (player === CIRCLE_CLASS) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
+}
+
+
+function getAvailableSpots(board) {
+  return board.reduce((acc, cell, index) => {
+    if (!cell.classList.contains(X_CLASS) && !cell.classList.contains(CIRCLE_CLASS)) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
 }
